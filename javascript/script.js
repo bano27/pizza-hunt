fbf8751d520d0c229c0b26e092be6356a9981cec
bano27 <thairukibe2798@gmail.com>
2020-10-19 03:54:58 +0300

    radio values included in script file

--- javascript/script.js
+++ javascript/script.js
@@ -7,6 +7,9 @@ var w = document.getElementById('toppings').value;
 var x = document.getElementById('size').value;
 var y = document.getElementById('crusts').value;
 var z = document.getElementById('orders').value;
+var yes = document.getElementById('YES').value;
+var no = document.getElementById('NO').value;
+
 
 
 alert("YOUR ORDER WAS"+ " "+ v + " "+ w + " "+ x + " "+ y + " "+ z + " " + "TOTAL COST OF YOUR ORDER IS") }
