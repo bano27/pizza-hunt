97be8d483a7a107c30e19d67c5ba55ee07b692bb
bano27 <thairukibe2798@gmail.com>
2020-10-19 03:38:52 +0300

    alert in script

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
-function submitted(){alert("thank you for making an order") }
+function submitted(){
+
+const myForm = document.getElementById("myForm")
+
+var v = document.getElementById('type').value;
+var w = document.getElementById('toppings').value;
+var x = document.getElementById('size').value;
+var y = document.getElementById('crusts').value;
+var z = document.getElementById('orders').value;
+
+
+alert("TOTAL COST OF YOUR ORDER IS") }
