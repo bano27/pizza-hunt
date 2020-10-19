b5851c421b9181588867a87fb216d1b53c882eed
bano27 <thairukibe2798@gmail.com>
2020-10-20 02:50:35 +0300

    if statement and a div container added

--- javascript/script.js
+++ javascript/script.js
@@ -1,39 +1,189 @@
-function submitted(){
+//ordering form
 
 const myForm = document.getElementById("myForm")
+
+function submitted(){
 
 var v = document.getElementById('type').value;
 var w = document.getElementById('toppings').value;
 var x = document.getElementById('size').value;
 var y = document.getElementById('crusts').value;
-var z = document.getElementById('orders').value;
-var yes = document.getElementById('YES').value;
-var no = document.getElementById('NO').value;
+var z = parseInt(document.getElementById('orders').value);
+var delivery = document.getElementsByName('Deliveries');
+var hSmall = 500;
+var hMedium = 650;
+var hLarge = 800;
+var cSmall = 500;
+var cMedium = 600;
+var cLarge = 700;
+var vSmall = 400;
+var vMedium = 500;
+var vLarge = 600;
+var a = 200;
+var b = 100;
+var c = 50;
+var d = 500;
+var price;
 
-if(v === "Hunter's Delight" && x === "Hunter's Delight Large"){price = 800;}
-else if (v === "Hunter's Delight" && x === "Hunter's Delight Medium") {
-  price = 650;
+if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Small" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( hSmall * z ) + c + d );
 }
-else if (v === "Hunter's Delight" && x === "Hunter's Delight Small") {
-  price = 500;
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Medium" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( hMedium * z ) + c + d );
 }
-break;
-if(v === "Chicken Raid" && x === "Chicken Raid Large"){price = 700;}
-else if (v === "Chicken Raid" && x === "Chicken Raid Medium") {
-  price = 600;
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Large" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( hLarge * z ) + c + d );
 }
-else if (v === "Chicken Raid" && x === "Chicken Raid Small") {
-  price = 500;
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Small" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( cSmall * z ) + c + d );
 }
-break;
-if(v === "Veggie Seekers" && x === "Veggie Seekers Large"){price = 600;}
-else if (v === "Veggie Seekers" && x === "Veggie Seekers Medium") {
-  price = 500;
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Medium" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( cMedium * z ) + c + d );
 }
-else if (v === "Veggie Seekers" && x === "Veggie Seekers Small") {
-  price = 400;
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Large" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( cLarge * z ) + c + d );
 }
-
-
-
-alert("YOUR ORDER WAS"+ " "+ v + " "+ w + " "+ x + " "+ y + " "+ z + " " + "TOTAL COST OF YOUR ORDER IS") }
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Small" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( vSmall * z ) + c + d );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Medium" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( vMedium * z ) + c + d );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Large" && y == "Fisherman Smile" && delivery[0].checked) {
+  price = ( ( vLarge * z ) + c + d );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Small" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( hSmall * z ) + a + d );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Medium" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( hMedium * z ) + a + d );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Large" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( hLarge * z ) + a + d );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Small" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( cSmall * z ) + a + d );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Medium" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( cMedium * z ) + a + d );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Large" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( cLarge * z ) + a + d );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Small" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( vSmall * z ) + a + d );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Medium" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( vMedium * z ) + a + d );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Large" && y == "Canine Nightmare" && delivery[0].checked) {
+  price = ( ( vLarge * z ) + a + d );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Small" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( hSmall * z ) + b + d );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Medium" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( hMedium * z ) + b + d );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Large" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( hLarge * z ) + b + d );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Small" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( cSmall * z ) + b + d );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Medium" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( cMedium * z ) + b + d );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Large" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( cLarge * z ) + b + d );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Small" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( vSmall * z ) + b + d );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Medium" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( vMedium * z ) + b + d );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Large" && y == "Rhino Hide" && delivery[0].checked) {
+  price = ( ( vLarge * z ) + b + d );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Small" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( hSmall * z ) + c );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Medium" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( hMedium * z ) + c );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Large" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( hLarge * z ) + c );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Small" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( cSmall * z ) + c );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Medium" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( cMedium * z ) + c );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Large" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( cLarge * z ) + c );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Small" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( vSmall * z ) + c );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Medium" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( vMedium * z ) + c );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Large" && y == "Fisherman Smile" && delivery[1].checked) {
+  price = ( ( vLarge * z ) + c );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Small" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( hSmall * z ) + a );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Medium" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( hMedium * z ) + a );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Large" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( hLarge * z ) + a );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Small" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( cSmall * z ) + a );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Medium" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( cMedium * z ) + a );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Large" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( cLarge * z ) + a );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Small" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( vSmall * z ) + a );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Medium" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( vMedium * z ) + a );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Large" && y == "Canine Nightmare" && delivery[1].checked) {
+  price = ( ( vLarge * z ) + a );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Small" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( hSmall * z ) + b );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Medium" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( hMedium * z ) + b );
+}
+else if ( w == "Hunter's Delight toppings" && x == "Hunter's Delight Large" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( hLarge * z ) + b );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Small" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( cSmall * z ) + b );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Medium" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( cMedium * z ) + b );
+}
+else if ( w == "Chicken Raid toppings" && x == "Chicken Raid Large" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( cLarge * z ) + b );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Small" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( vSmall * z ) + b );
+}
+else if ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Medium" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( vMedium * z ) + b );
+}
+else ( w == "Veggie Seekers toppings" && x == "Veggie Seekers Large" && y == "Rhino Hide" && delivery[1].checked) {
+  price = ( ( vLarge * z ) + b );
+}
